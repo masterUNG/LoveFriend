fdf59cb9f249a0d6d5ef8a1d7c0cddde7ed9dec4
MasterUNG <phrombutr@gmail.com>
2018-06-06 05:20:00 +0700

    update 6June

--- app/src/main/java/masterung/androidthai/in/th/lovefriend/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/lovefriend/fragment/RegisterFragment.java
@@ -10,17 +10,23 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import masterung.androidthai.in.th.lovefriend.MainActivity;
 import masterung.androidthai.in.th.lovefriend.R;
+import masterung.androidthai.in.th.lovefriend.utility.MasterAlert;
 
 public class RegisterFragment extends Fragment {
 
     private Uri uri;
     private ImageView imageView;
+    private boolean photoABoolean = true;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -36,6 +42,52 @@ public class RegisterFragment extends Fragment {
     }   // Main Method
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.itemUpload) {
+            uploadToServer();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadToServer() {
+
+        MasterAlert masterAlert = new MasterAlert(getActivity());
+
+        EditText nameEditText = getView().findViewById(R.id.edtName);
+        EditText userEditText = getView().findViewById(R.id.edtUser);
+        EditText passwordEditText = getView().findViewById(R.id.edtPassword);
+
+        String nameString = nameEditText.getText().toString().trim();
+        String userString = userEditText.getText().toString().trim();
+        String passwordString = passwordEditText.getText().toString().trim();
+
+
+//        Check Choose Photo
+        if (photoABoolean) {
+            masterAlert.normalDialog("Choose Avata",
+                    "Please Choose Avata");
+        } else if (nameString.isEmpty() ||
+                userString.isEmpty() ||
+                passwordString.isEmpty()) {
+            masterAlert.normalDialog("Have Space",
+                    "Please Fill Every Blank");
+        } else {
+
+        }
+
+    }   // uploadToServer
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+    }
+
+    @Override
     public void onActivityResult(int requestCode,
                                  int resultCode,
                                  Intent data) {
@@ -43,17 +95,22 @@ public class RegisterFragment extends Fragment {
 
         if (resultCode == getActivity().RESULT_OK) {
 
+            photoABoolean = false;
+
             uri = data.getData();
             try {
+
+                BitmapFactory.Options options = new BitmapFactory.Options();
+                options.inSampleSize = 8;
 
                 Bitmap bitmap = BitmapFactory
                         .decodeStream(getActivity()
                                 .getContentResolver()
-                                .openInputStream(uri));
+                                .openInputStream(uri), null, options);
 
-                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,
-                        800, 600, true);
-                imageView.setImageBitmap(bitmap1);
+//                Bitmap bitmap1 = Bitmap.createScaledBitmap(bitmap,
+//                        800, 600, true);
+                imageView.setImageBitmap(bitmap);
 
             } catch (Exception e) {
                 e.printStackTrace();
@@ -92,6 +149,9 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+
+        setHasOptionsMenu(true);
+
     }
 
     @Nullable
