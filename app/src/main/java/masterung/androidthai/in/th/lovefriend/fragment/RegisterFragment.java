e6a61a4b72c877b8610017a61b02c5bc787380c0
MasterUNG <phrombutr@gmail.com>
2018-06-10 17:29:56 +0700

    update 10June

--- app/src/main/java/masterung/androidthai/in/th/lovefriend/fragment/RegisterFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/lovefriend/fragment/RegisterFragment.java
@@ -1,14 +1,19 @@
 package masterung.androidthai.in.th.lovefriend.fragment;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,16 +22,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferListener;
 import masterung.androidthai.in.th.lovefriend.MainActivity;
 import masterung.androidthai.in.th.lovefriend.R;
 import masterung.androidthai.in.th.lovefriend.utility.MasterAlert;
+import masterung.androidthai.in.th.lovefriend.utility.MyConstant;
+import masterung.androidthai.in.th.lovefriend.utility.PostUserToServer;
 
 public class RegisterFragment extends Fragment {
 
     private Uri uri;
     private ImageView imageView;
     private boolean photoABoolean = true;
+    private ProgressDialog progressDialog;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -75,9 +88,114 @@ public class RegisterFragment extends Fragment {
                     "Please Fill Every Blank");
         } else {
 
+//            Upload Photo To Server
+            uploadPhoth();
+
+//            upload Value to MySQL
+            uploadValue(nameString, userString, passwordString);
+
+
         }
 
     }   // uploadToServer
+
+    private void uploadValue(String nameString,
+                             String userString,
+                             String passwordString) {
+
+        String avataString =  "http://androidthai.in.th/love/Avata/" +
+                findPathString().substring(findPathString().lastIndexOf("/"));
+        Log.d("10JuneV1", "avataString ==> " + avataString);
+
+        MyConstant myConstant = new MyConstant();
+        MasterAlert masterAlert = new MasterAlert(getActivity());
+
+        try {
+
+            PostUserToServer postUserToServer = new PostUserToServer(getActivity());
+            postUserToServer.execute(
+                    nameString,
+                    userString,
+                    passwordString,
+                    avataString,
+                    myConstant.getUrlAddData());
+
+            if (Boolean.parseBoolean(postUserToServer.get())) {
+                Toast.makeText(getActivity(), "Register Success",
+                        Toast.LENGTH_SHORT).show();
+                getActivity().getSupportFragmentManager().popBackStack();
+            } else {
+                masterAlert.normalDialog("Cannot Register",
+                        "Please Try Again");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void uploadPhoth() {
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setTitle("Upload Photo");
+        progressDialog.setMessage("Please Wait Few Minus ...");
+
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        FTPClient ftpClient = new FTPClient();
+        MyConstant myConstant = new MyConstant();
+        try {
+
+            ftpClient.connect(myConstant.getHostFTPString(), myConstant.getPortAnInt());
+            ftpClient.login(myConstant.getUserFTPString(), myConstant.getPasswordString());
+            ftpClient.setType(FTPClient.TYPE_BINARY);
+            ftpClient.changeDirectory("Avata");
+            ftpClient.upload(findFild(), new MyFTPDataTransferListener());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.d("10JuntV1", "e ==> " + e.toString());
+            try {
+                ftpClient.disconnect(true);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+                Log.d("10JuntV1", "e1 ==> " + e1.toString());
+            }
+        }
+
+
+    }
+
+    private File findFild() {
+
+        String pathPhoto = findPathString();
+        File file = new File(pathPhoto);
+
+        return file;
+    }
+
+    private String findPathString() {
+
+        String pathPhoto = null;
+
+        String[] strings = new String[]{MediaStore.Images.Media.DATA};
+        Cursor cursor = getActivity().getContentResolver().query(uri, strings,
+                null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            int i = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            pathPhoto = cursor.getString(i);
+        } else {
+            pathPhoto = uri.getPath();
+        }
+
+        return pathPhoto;
+    }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
@@ -163,4 +281,37 @@ public class RegisterFragment extends Fragment {
                 container, false);
         return view;
     }
-}
+
+    public class MyFTPDataTransferListener implements FTPDataTransferListener{
+
+
+
+        @Override
+        public void started() {
+            progressDialog.show();
+            Log.d("10JuneV1", "Upload Start");
+        }
+
+        @Override
+        public void transferred(int i) {
+
+        }
+
+        @Override
+        public void completed() {
+            progressDialog.dismiss();
+            Log.d("10JuneV1", "Upload Stop");
+        }
+
+        @Override
+        public void aborted() {
+
+        }
+
+        @Override
+        public void failed() {
+
+        }
+    }   // MyFTPDataTranferListener
+
+}   // Main Class
---
+++ app/src/main/java/masterung/androidthai/in/th/lovefriend/utility/MyConstant.java
@@ -0,0 +1,31 @@
+package masterung.androidthai.in.th.lovefriend.utility;
+
+public class MyConstant {
+
+    private String hostFTPString = "ftp.androidthai.in.th";
+    private String userFTPString = "love@androidthai.in.th";
+    private String passwordString = "Abc12345";
+    private int portAnInt = 21;
+
+    private String urlAddData = "http://androidthai.in.th/love/addData.php";
+
+    public String getUrlAddData() {
+        return urlAddData;
+    }
+
+    public String getHostFTPString() {
+        return hostFTPString;
+    }
+
+    public String getUserFTPString() {
+        return userFTPString;
+    }
+
+    public String getPasswordString() {
+        return passwordString;
+    }
+
+    public int getPortAnInt() {
+        return portAnInt;
+    }
+}
